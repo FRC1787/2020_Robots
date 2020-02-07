35d4c8d046975b10111920211bedf957d543544c
ChimpsterMan <chimpsterguy@gmail.com>
2020-02-07 16:40:07 -0500

    breaker updates by Erik

--- 2020_Robots/src/main/java/frc/robot/subsystems/Intake.java
+++ 2020_Robots/src/main/java/frc/robot/subsystems/Intake.java
@@ -7,6 +7,8 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Timer;
@@ -26,6 +28,20 @@ public class Intake extends SubsystemBase {
   public static Timer intakeTimer = new Timer();
   public static Timer ballTimer = new Timer();
 
+  //ball counting variables
+  //enter pin number u want
+  private static double breakerVoltageConstant = 40;
+  private static final AnalogInput succitvan2 = new AnalogInput(0);
+  public static double succitvan2Raw = succitvan2.getVoltage();
+
+  //jordan u need to configure and test different values for the analog pin, voltage constant, ball delay, and samples
+  private static int ballDelay = 20;
+  private static int MAX_BALLS = 5;
+  private static int SAMPLES = 10;
+  private static int ballDelayCounter;
+  private static int sampleIndex;
+  private static int[] samples;
+
   public static double proximity;
   public static boolean ballReady;
   public static double ballCount = 0;
@@ -34,13 +50,32 @@ public class Intake extends SubsystemBase {
   public static boolean ballStuck;
 
   public Intake() {
+    this(0);
+  }
+
+  public Intake(int ballsLoaded){
+    Intake.ballCount = ballsLoaded;
+    Intake.ballDelayCounter = Intake.ballDelay;
+    Intake.sampleIndex = 0;
+    Intake.samples = new int[Intake.SAMPLES];
   }
 
   public void intakeStage1(double setSpeed){
     intakeDrum.set(setSpeed);
   }
 
+  public static boolean getBreakerState(){
+    return (succitvan2.getVoltage() > Intake.breakerVoltageConstant);
+  }
+
   public static void ballCheck() {
+    //check if the balls are full
+    if (ballCount >= MAX_BALLS){
+      ballFull = true;
+    } else {
+      ballFull = false;
+    }
+
     if (Intake.ballReady) {
       ballTimer.start();
       if (ballTimer.get() >= .5 && Intake.ballReady) {
@@ -48,9 +83,42 @@ public class Intake extends SubsystemBase {
       }
     }
     else if (Intake.proximity > 20) {
-    Intake.ballStuck = false;
-    ballTimer.stop();
-    ballTimer.reset();
+      Intake.ballStuck = false;
+      ballTimer.stop();
+      ballTimer.reset();
+    }
+
+    //the breaker code
+    //The counter to allow a ball to clear the breaker after detection you must configure by hand
+    if (Intake.ballDelayCounter >= Intake.ballDelay){
+      if (Intake.sampleIndex < Intake.SAMPLES && !Intake.ballFull){
+        //if the breaker is activated
+        if (!getBreakerState()){
+          Intake.samples[sampleIndex] = 1;
+        } else {
+          Intake.samples[sampleIndex] = -1;
+        }
+
+        Intake.sampleIndex += 1;
+      } else {
+        //iterate through samples and if the average is high enough then add ball
+        int sum = 0;
+
+        for (int i: samples){
+          sum += i;
+        }
+
+        if (sum > 0){
+          Intake.ballCount += 1;
+        }
+
+        //reset stuff
+        Intake.samples = new int[SAMPLES];
+        Intake.sampleIndex = 0;
+        Intake.ballDelayCounter = 0;
+      }
+    } else {
+      Intake.ballDelayCounter += 1;
     }
   }
 
@@ -74,6 +142,10 @@ public class Intake extends SubsystemBase {
 
   @Override
   public void periodic() {
+    //functions
+    ballCheck();
+
+    //setters
     Intake.proximity = colorSensor.getProximity();
     Intake.ballReady = (Intake.proximity > 20);
     Intake.ballFull = (Intake.ballCount > 5);
